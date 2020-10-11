dc42ff6831b3d4b214436cf4e9893fa30acf0a5d
Lasse Skaalum <lskaal19@student.aau.dk>
2020-10-11 22:09:25 +0200

    Changed to reaction instead of message

--- Models/MsgContains/YEP/Yep.js
+++ Models/MsgContains/YEP/Yep.js
@@ -3,7 +3,8 @@ const Model = require(`../../AbstractModels/Model`);
 class Yep extends Model {
   handle() {
     const yep = global.bot.emojis.cache.find((emoji) => emoji.name === `YEP`);
-    this.msg.channel.send(`${yep}`);
+    // this.msg.channel.send(`${yep}`);
+    this.msg.react(yep);
   }
 }
 
